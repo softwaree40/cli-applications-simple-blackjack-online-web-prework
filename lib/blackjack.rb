f9521ab8f6d40cba9fb9398c2a26b149a88c8792
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-02 01:09:31 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -42,7 +42,8 @@ end
 def initial_round
 
       sum = deal_card + deal_card
-      puts "/Your cards add up to /"
+      #puts "/Your cards add up to /"
+      display_card_total(sum)
     
    # code #deal_card here 
      sum 
@@ -75,6 +76,7 @@ def hit?(card_total)
         hit?(card_total)
         #binding.pry
   end
+    card_total
 end
 
 def invalid_command
