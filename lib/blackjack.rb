f1e96f895b3875259540ef4641a4c53575ece893
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-02 00:45:33 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -92,8 +92,8 @@ def runner
   card_total = initial_round
   card_total = hit?(card_total)
   if initial_round > 21
-    end_game(card_total)
     display_card_total(card_total)
+    end_game(card_total)
   end
 end
   
